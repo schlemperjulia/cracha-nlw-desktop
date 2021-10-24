0a8ffdd17ccd615e4901b20f4370dfdf0d925aa5
Julia Schlemper <71457969+schlemperjulia@users.noreply.github.com>
2021-10-24 11:44:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'schlemperjulia',
+  linkedin: 'in/julia-schlemper-da-silva-434881177',
+  instagram: 'juliaschlemper',
+  facebook: 'profile.php?id=100009337988005',
+  twitter: 'repmelhcs26'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
